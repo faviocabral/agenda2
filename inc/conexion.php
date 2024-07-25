ecf0731375c0f622d9954049d91f167034d04f00
faviocabral <faviocabral@gmail.com>
2024-07-25 19:05:45 -0400

    con

--- inc/conexion.php
+++ inc/conexion.php
@@ -12,6 +12,7 @@ $dbpuerto 	= $puerto_;
 $dbusuario	= $usuario_;
 $dbcontrasena = $contrasena_;
 $db			= $base_;
+$db2			= $base2_;
 $con		= pg_connect("host=$dbhost port=$dbpuerto password=$dbcontrasena user=$dbusuario dbname=$db") or die( 'No se pudo conectar'.$con);
 $con2		= pg_connect("host=$dbhost port=$dbpuerto password=$dbcontrasena user=$dbusuario dbname=$db2") or die( 'No se pudo conectar'.$con2);
 $user = "";
